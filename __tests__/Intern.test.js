9b405e702bc159f4afdcb5bbf18a5acb58662e58
zachary <zacharyberdell@gmail.com>
2021-11-01 18:06:11 -0600

    Test building completed.

--- __tests__/Intern.test.js
+++ __tests__/Intern.test.js
@@ -1,1 +1,15 @@
 const Intern = require("../lib/Intern");
+
+describe("Intern", () => {
+    it("Can set a school based on a constructor", () => {
+        const school = "University of Denver";
+        const empSchool = new Intern("name", 1, "email@example.com", school);
+        expect(empSchool.school).toEqual(school);
+    });
+
+    it("Should have getrole() return \"Intern\"", () => {
+        const role = "Intern";
+        const empRole = new Intern("name", 1, "email@example.com", "school");
+        expect(empRole.getRole()).toEqual(role);
+    });
+});
--- __tests__/Manager.test.js
+++ __tests__/Manager.test.js
@@ -1,1 +1,15 @@
 const Manager = require("../lib/Manager");
+
+describe("Manager", () => {
+    it("Can set an office number based on a constructor", () => {
+       const officeNumber = "12";
+       const empOffice = new Manager("name", 1, "email@example.com", officeNumber);
+       expect(empOffice.office).toEqual(officeNumber);
+    });
+
+    it("Should have getRole() return \"Manager\"", () => {
+        const role = "Manager";
+        const empRole = new Manager("name", 1, "email@example.com", 1);
+        expect(empRole.getRole()).toEqual(role);
+    });   
+});
