50bc169e05ad064cdcacf4ba42cff00b752765b1
zachary <zacharyberdell@gmail.com>
2021-11-03 09:06:04 -0600

    Added requires to the classes

--- index.js
+++ index.js
@@ -2,4 +2,33 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
 const path = require("path");
-const employee = require("employee");
+const Manager = require("./lib/Manager");
+const Intern = require("./lib/Intern");
+const Engineer = require("./lib/Engineer");
+
+//Variable so the user can decide if they want to add a new member or not
+var addNew = true;
+
+//Runs while the user still wants to add a new member
+while(addNew){
+    inquirer
+    .prompt({
+        type: "list",
+        message: "Would you like to add a new member?",
+        name: "userCheck",
+        choices: ["Yes", "No"]
+    })
+    .then((data) => {
+        if(data.userCheck == "Yes"){
+            questionAsker();
+        }
+        else{
+            addNew = false;
+        }
+    })
+}
+
+//Function for asking the questions to generate the html
+function questionAsker() {
+    
+}
