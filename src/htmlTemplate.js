27df9296620688c92e8d4858a4b8d941c14e3c94
zachary <zacharyberdell@gmail.com>
2021-11-06 19:31:45 -0600

    Html generation edited.

--- src/htmlTemplate.js
+++ src/htmlTemplate.js
@@ -76,30 +76,30 @@ module.exports = team => {
     <!DOCTYPE html>
 <html lang="en">
 <head>
+    <title>Team Profiles</title>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <meta http-equiv="X-UA-Compatible" content="ie=edge" />
-    <title>My Team</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
         integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <link rel="stylesheet" href="style.css">
     <script src="https://kit.fontawesome.com/c502137733.js"></script>
 </head>
 <body>
-    <div class="container-fluid">
+    <header class="container-fluid">
         <div class="row">
-            <div class="col-12 jumbotron mb-3 team-heading">
-                <h1 class="text-center">My Team</h1>
+            <div class="col-12 jumbotron mb-3 team-heading bg-primary">
+                <h1 class="text-center text-light">Team Profiles</h1>
             </div>
         </div>
-    </div>
-    <div class="container">
+    </header>
+    <main class="container">
         <div class="row">
             <div class="team-area col-12 d-flex justify-content-center">
                 ${generateTeam(team)}
             </div>
         </div>
-    </div>
+    </main>
 </body>
 </html>
     `;
